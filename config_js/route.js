20facebeb53fa3688ca4a6c1c1369ef0ad01809c
Felix Tioh <felixsagitta@gmail.com>
2013-11-25 16:13:10 +0800

    hello

---
+++ config_js/route.js
@@ -0,0 +1,18 @@
+angular.module('pages').config(['$routeProvider', 'WardenProvider', function($routeProvider, WardenProvider) {
+        return WardenProvider.simplify($routeProvider)
+            .set_template_prefix('views/pages')
+            .omit_controller()
+            .when('home', {omitController: false})
+            .when('about')
+            .when('contact', {omitController: false})
+            .when('check_email')
+            .when('faq')
+            .when('terms')
+            .when('privacy')
+            .when('blog')
+            .when('why_list_our_business')
+            .when('advertise')
+            .when('newsletter');
+    }
+]);
+
